03698669a1ecf23c0253403c8a2f6d014d7f20f9
Karl Secen <karl.secen@gmail.com>
2020-07-29 17:52:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "./useForm";
+import { useFetch } from "./useFetch";
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [count, setCount] = useState(0)
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  /*   useEffect(() => {
+    console.log('render')
+    return () => {
+      console.log('unmount')
+    }
+  }, [values.email]) */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{loading ? "loading.." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c+1)}>increment</button>
+      <input
+        type="email"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
